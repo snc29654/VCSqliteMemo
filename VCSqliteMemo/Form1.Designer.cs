766af2a58c167305cdfc4a2845cb004422eebf4f
snc29654 <snc29654l@nifty.com>
2022-03-04 22:54:07 +0900

    プロジェクト ファイルを追加します。

---
+++ VCSqliteMemo/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿
+namespace VCSqliteMemo
+{
+    partial class 備忘録
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(383, 363);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 434);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(383, 132);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(444, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(93, 19);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(477, 43);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(55, 19);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(435, 94);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(357, 472);
+            this.textBox4.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(811, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 31);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "テーブル作成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(811, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 31);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "サンプル登録";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(811, 262);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 31);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "テーブル削除";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(28, 18);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 31);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "全データ表示";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(435, 6);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(97, 31);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "レコード追加";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(327, 570);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(81, 32);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "クリア";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(700, 568);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(92, 36);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "クリア";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click_1);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(587, 6);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(102, 31);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "レコード読み出し";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click_1);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(587, 55);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(102, 31);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "レコード更新";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click_1);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(25, 568);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(123, 37);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "種別検索";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click_1);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(168, 18);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(134, 31);
+            this.button11.TabIndex = 15;
+            this.button11.Text = "レコード削除";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click_1);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(189, 579);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(57, 19);
+            this.textBox5.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(442, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "種別";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(154, 582);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "種別";
+            // 
+            // 備忘録
+            // 
+            this.ClientSize = new System.Drawing.Size(962, 630);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "備忘録";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ VCSqliteMemo/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+
+namespace VCSqliteMemo
+{
+    public partial class 備忘録 : Form
+    {
+        public 備忘録()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // テーブル名が存在しなければ作成する
+            StringBuilder query = new StringBuilder();
+            query.Clear();
+            query.Append("CREATE TABLE IF NOT EXISTS PURCHASELIST (");
+            query.Append(" NO INTEGER PRIMARY KEY AUTOINCREMENT");
+            query.Append(" ,DATETIME TEXT NOT NULL");
+            query.Append(" ,NAME TEXT NOT NULL");
+            query.Append(" ,CONTENT TEXT NOT NULL");
+            query.Append(")");
+
+            // クエリー実行
+            ExecuteNonQuery(query.ToString());
+
+        }
+
+        private void ExecuteNonQuery(string query)
+        {
+            try
+            {
+                // 接続先を指定
+                using (var conn = new SQLiteConnection("Data Source=Memo.db"))
+                using (var command = conn.CreateCommand())
+                {
+                    // 接続
+                    conn.Open();
+
+                    // コマンドの実行処理
+                    command.CommandText = query;
+                    command.ExecuteNonQuery();
+                    //var value = command.ExecuteNonQuery();
+                    //MessageBox.Show($"更新されたレコード数は {value} です。");
+                }
+            }
+            catch (Exception ex)
+            {
+                //例外が発生した時はメッセージボックスを表示
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void InsertRecord(string datetime, string name, string content)
+        {
+            // レコードの登録
+            var query = "INSERT INTO PURCHASELIST (DATETIME,NAME,CONTENT) VALUES (" +
+                $"'{datetime}','{name}','{content}')";
+
+            // クエリー実行
+            ExecuteNonQuery(query.ToString());
+        }
+
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            InsertRecord(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), "日記", "記事１");
+            InsertRecord(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), "ニュース", "記事２");
+            InsertRecord(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), "メモ", "記事３");
+            InsertRecord(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), "住所", "記事４");
+
+        }
+        private void DropTable()
+        {
+            // テーブルの削除
+            var query = "DROP TABLE PURCHASELIST";
+
+            // クエリー実行
+            ExecuteNonQuery(query.ToString());
+        }
+        private void SerachRecordDataAll()
+        {
+            SQLiteConnection con = new SQLiteConnection("Data Source=Memo.db");
+            con.Open();
+            try
+            {
+                // 列数を指定します。
+                dataGridView1.ColumnCount = 5;
+
+                // 列幅を表の表示領域いっぱいに調整します。
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+                // 列名を指定します。
+                dataGridView1.Columns[0].HeaderText = "行番";
+                dataGridView1.Columns[1].HeaderText = "レコード番号";
+                dataGridView1.Columns[2].HeaderText = "日時";
+                dataGridView1.Columns[3].HeaderText = "種別";
+                dataGridView1.Columns[4].HeaderText = "内容";
+
+                string sql = "SELECT * FROM PURCHASELIST  ORDER BY NO ASC";
+
+                SQLiteCommand com = new SQLiteCommand(sql, con);
+                SQLiteDataReader sdr = com.ExecuteReader();
+                int row = 0;
+                while (sdr.Read() == true)
+                {
+                    textBox1.Text += sdr["NO"].ToString();
+                    dataGridView1.Rows.Add(row, sdr["NO"].ToString(), sdr["DATETIME"].ToString(), sdr["NAME"].ToString(), sdr["CONTENT"].ToString());
+                    textBox1.Text += " : ";
+                    textBox1.Text += sdr["DATETIME"].ToString();
+                    textBox1.Text += " : ";
+                    textBox1.Text += sdr["NAME"].ToString();
+                    textBox1.Text += " : ";
+                    textBox1.Text += sdr["CONTENT"].ToString();
+                    textBox1.Text += "\r\n";
+                    row++;
+                }
+
+
+
+
+                sdr.Close();
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+
+        private void SerachRecordDataKind(string column, string word)
+        {
+            SQLiteConnection con = new SQLiteConnection("Data Source=Memo.db");
+            con.Open();
+            try
+            {
+
+                // 列数を指定します。
+                dataGridView1.ColumnCount = 5;
+
+                // 列幅を表の表示領域いっぱいに調整します。
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+                // 列名を指定します。
+                dataGridView1.Columns[0].HeaderText = "行番";
+                dataGridView1.Columns[1].HeaderText = "レコード番号";
+                dataGridView1.Columns[2].HeaderText = "日時";
+                dataGridView1.Columns[3].HeaderText = "種別";
+                dataGridView1.Columns[4].HeaderText = "内容";
+
+                string sql = "SELECT * FROM PURCHASELIST WHERE " +
+                $"{column} = '{word}' ORDER BY NO ASC";
+
+
+                SQLiteCommand com = new SQLiteCommand(sql, con);
+                SQLiteDataReader sdr = com.ExecuteReader();
+                int row = 0;
+                while (sdr.Read() == true)
+                {
+                    dataGridView1.Rows.Add(row, sdr["NO"].ToString(), sdr["DATETIME"].ToString(), sdr["NAME"].ToString(), sdr["CONTENT"].ToString());
+                    textBox1.Text += sdr["NO"].ToString();
+                    textBox1.Text += " : ";
+                    textBox1.Text += sdr["DATETIME"].ToString();
+                    textBox1.Text += " : ";
+                    textBox1.Text += sdr["NAME"].ToString();
+                    textBox1.Text += " : ";
+                    textBox1.Text += sdr["CONTENT"].ToString();
+                    textBox1.Text += "\r\n";
+                    row++;
+                }
+                sdr.Close();
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            DropTable();
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            SerachRecordDataAll();
+
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            InsertRecord(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), textBox3.Text, textBox4.Text);
+
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+
+        }
+        public void SerachRecordData2(string column, string word)
+        {
+
+            SQLiteConnection con = new SQLiteConnection("Data Source=Memo.db");
+            con.Open();
+            try
+            {
+                string sql = "SELECT * FROM PURCHASELIST WHERE " +
+                $"{column} = '{word}' ORDER BY NO ASC";
+
+                SQLiteCommand com = new SQLiteCommand(sql, con);
+                SQLiteDataReader sdr = com.ExecuteReader();
+                while (sdr.Read() == true)
+                {
+                    textBox2.Text = sdr["NO"].ToString();
+                    textBox3.Text = sdr["NAME"].ToString();
+                    textBox4.Text = sdr["CONTENT"].ToString();
+                }
+                sdr.Close();
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+        private void UpdateRecord(int no, string datetime, string name, string content)
+        {
+            // レコードの登録
+            var query = $"UPDATE PURCHASELIST SET DATETIME = '{datetime}', NAME = '{name}', CONTENT = '{content}' " +
+                $"WHERE NO = {no};";
+
+            // クエリー実行
+            ExecuteNonQuery(query.ToString());
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+
+        }
+
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+
+            string s = dataGridView1[dataGridView1.CurrentCell.ColumnIndex + 1, dataGridView1.CurrentCell.RowIndex].Value.ToString();
+            textBox2.Text = s;
+            string s1 = dataGridView1[dataGridView1.CurrentCell.ColumnIndex + 3, dataGridView1.CurrentCell.RowIndex].Value.ToString();
+            textBox3.Text = s1;
+            string s2 = dataGridView1[dataGridView1.CurrentCell.ColumnIndex + 4, dataGridView1.CurrentCell.RowIndex].Value.ToString();
+            textBox4.Text = s2;
+
+
+            //SerachRecordData2("NO", textBox2.Text);
+
+        }
+
+        private void button9_Click_1(object sender, EventArgs e)
+        {
+
+
+            UpdateRecord(int.Parse(textBox2.Text), DateTime.Now.ToString(), textBox3.Text, textBox4.Text);
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void DeleteRecord(string column, string word)
+        {
+            // レコードの削除
+            var query = "DELETE FROM PURCHASELIST WHERE " +
+                $"{column} = '{word}'";
+
+            // クエリー実行
+            ExecuteNonQuery(query.ToString());
+        }
+
+        private void button10_Click_1(object sender, EventArgs e)
+        {
+            SerachRecordDataKind("NAME", textBox5.Text);
+
+        }
+
+        private void button11_Click_1(object sender, EventArgs e)
+        {
+
+            string s = dataGridView1[dataGridView1.CurrentCell.ColumnIndex + 1, dataGridView1.CurrentCell.RowIndex].Value.ToString();
+            DeleteRecord("NO", s);
+
+
+            textBox1.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+
+            SerachRecordDataAll();
+
+
+        }
+    }
+
+}
